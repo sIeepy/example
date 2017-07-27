6eda2c2c2d40df3122ea5153aa0fc1d79465d5f5
sleepy <biegacz1993@wp.pl>
2017-07-27 19:53:20 +0200

    micropost add, show. TODO micropost spec

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,24 @@
 class MicropostsController < ApplicationController
   expose(:microposts) { Micropost.all }
-  expose(:micropost)
-  
-  def show; end
+  expose(:micropost, attributes: :micropost_params)
+
+  def create
+    micropost = current_user.microposts.build(micropost_params)
+    if micropost.save
+      redirect_to microposts_path, notice: I18n.t('shared.created', resource: 'Micropost')
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    micropost.destroy
+    redirect_to microposts_path, notice: I18n.t('shared.deleted', resource: 'Micropost')
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :created_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'home#welcome'
+
+  resources :users do
+    resources :microposts
+  end
   resources :users
+  resources :microposts
 end
